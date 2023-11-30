ffb7983eef407ac78ebc46c8935afb5c397093c2
giop5 <giordanapistorio96@gmail.com>
2023-11-30 19:01:07 +0100

    sistemazione login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,30 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent {
-
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
-  Submit(){
-    console.log(this.loginForm.value)
+  // showInvalidInput: boolean = false;
+  // passwordInputType: string = 'password';
+
+  // showPassword() {
+  //   this.passwordInputType === 'password'
+  //     ? (this.passwordInputType = 'text')
+  //     : (this.passwordInputType = 'password');
+  // }
+
+  Submit() {
+    console.log(this.loginForm.value);
   }
 }
