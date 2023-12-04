4a2df51291d19eb875490bcdfe5ba1bcf7ec67a8
giop5 <giordanapistorio96@gmail.com>
2023-12-04 21:46:58 +0100

    commit app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
-import {RegistrationComponent} from "./components/registration/registration.component";
+import { LoginComponent} from "./components/login/login.component";
+import { RegistrationComponent} from "./components/registration/registration.component";
 import { NavbarFirstComponent } from './components/navbar-first/navbar-first.component';
 import {NavbarSecondComponent} from "./components/navbar-second/navbar-second.component";
 import {HomeComponent} from "./components/home/home.component";
