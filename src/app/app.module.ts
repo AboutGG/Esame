15bb232c8639155d43c4e7e8f373aa4ad614468e
giop5 <giordanapistorio96@gmail.com>
2023-12-04 20:51:52 +0100

    unione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,19 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RegistrationComponent} from './components/registration/registration.component';
 import {HttpClientModule} from "@angular/common/http";
 import { SearchComponent } from './components/search/search.component';
+import { NavbarFirstComponent } from './components/navbar-first/navbar-first.component';
+import { NavbarSecondComponent } from './components/navbar-second/navbar-second.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
+    NavbarSecondComponent,
     NavbarFirstComponent,
-    SearchComponent
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -27,29 +32,6 @@ import { SearchComponent } from './components/search/search.component';
   providers: [],
   bootstrap: [AppComponent]
 
-import { NavbarSecondComponent } from './components/navbar-second/navbar-second.component';
-import { NavbarFirstComponent } from './components/navbar-first/navbar-first.component';
-import { HomeComponent } from './components/home/home.component';
-
-
-@NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent,
-        RegistrationComponent,
-        NavbarSecondComponent,
-        NavbarFirstComponent,
-        HomeComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        NgbModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
